4bbff0136dc1a94fd5ae502ac56ff942f429cbc8
Rahmat Argyandha Aminuddin <argyandha25@gmail.com>
2026-03-12 11:11:02 +0700

    Update pegawai

--- inheritance/dosen/src/Pegawai.java
+++ inheritance/dosen/src/Pegawai.java
@@ -1,11 +1,24 @@
+import java.time.LocalDate;
+
 public class Pegawai {
 
     // parameter penyusun
    protected String NIP;
    protected String nama;
-   protected String tanggal_lahir;
+   protected LocalDate tanggal_lahir;
    protected int TMT;
-   protected int gajipokok;
+   protected double gajipokok;
+   protected int BUP;
+
+   // konstruktor
+   public Pegawai (String NIP, String nama, LocalDate tanggal_lahir, int TMT, int gajipokok, int BUP) {
+          this.NIP = NIP;
+          this.nama = nama;
+          this.tanggal_lahir = tanggal_lahir;
+          this.TMT = TMT;
+          this.gajipokok = gajipokok;
+          this.BUP = BUP;
+   }
 
    // getter dan setter
    public String getNIP() {
@@ -14,16 +27,29 @@ public class Pegawai {
    public String getNama() {
         return this.nama;
    }
-   public String getTanggal_lahir() {
+   public LocalDate getTanggal_lahir() {
         return this.tanggal_lahir;
    }
    public int getTMT() {
         return this.TMT; 
    }
-   public int getGajiPokok() {
+   public double getGajiPokok() {
         return this.gajipokok;
    }
-   
+   public int getBUP() {
+        return this.BUP;
+   }
+     public void printInfo()
+    {
+        System.out.println("NIP adalah " + NIP);
+        System.out.println("nama: " + nama);
+        System.out.println("tanggal lahir: " + tanggal_lahir);
+        System.out.println("TMT: " + TMT);
+        System.out.println("Gaji Pokok: " + gajipokok);
+        System.out.println("BUP: " + BUP);
+    }
+
+
 
 
 }
