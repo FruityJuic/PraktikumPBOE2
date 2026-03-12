df0d42e88c2a3c49b0fade087bb72e8154106c8b
Rahmat Argyandha Aminuddin <argyandha25@gmail.com>
2026-03-12 10:58:36 +0700

    Pertemuan Bangun Datar

---
+++ inheritance/dosen/src/Pegawai.java
@@ -0,0 +1,29 @@
+public class Pegawai {
+
+    // parameter penyusun
+   protected String NIP;
+   protected String nama;
+   protected String tanggal_lahir;
+   protected int TMT;
+   protected int gajipokok;
+
+   // getter dan setter
+   public String getNIP() {
+        return this.NIP;
+   }
+   public String getNama() {
+        return this.nama;
+   }
+   public String getTanggal_lahir() {
+        return this.tanggal_lahir;
+   }
+   public int getTMT() {
+        return this.TMT; 
+   }
+   public int getGajiPokok() {
+        return this.gajipokok;
+   }
+   
+
+
+}
