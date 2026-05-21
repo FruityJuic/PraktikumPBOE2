98eb5ca37ab959889ec51b74a8f90328529df274
Rahmat Argyandha Aminuddin <argyandha25@gmail.com>
2026-05-21 19:59:37 +0700

    kode baru

--- src/pbo12_24060124130061/MySQLPersonDAO.java
+++ src/pbo12_24060124130061/MySQLPersonDAO.java
@@ -8,20 +8,36 @@ public class MySQLPersonDAO implements PersonDAO {
     public void savePerson(Person person) throws Exception {
 
         String name = person.getName();
-
         String url = "jdbc:mysql://localhost:3306/pbo";
+        
+        // 1. REGISTRASI DRIVER SECARA MANUAL (Solusi untuk Eror "No suitable driver found")
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new Exception("Driver MySQL tidak ditemukan di Classpath/Modulepath! Periksa kembali library NetBeans kamu.", e);
+        }
 
-        Connection con = DriverManager.getConnection(url, “USERNAME_DB_KAMU”, “PASSWORD_KAMU”); 
+        // 2. KONEKSI KE DATABASE
+        Connection con = DriverManager.getConnection(
+                url,
+                "root",
+                ""
+        );
 
-        String query = "INSERT INTO person(name) VALUES('" + name + "')";
+        // 3. GUNAKAN PREPAREDSTATEMENT (Lebih aman dari SQL Injection & anti-eror tanda petik)
+        String query = "INSERT INTO person(name) VALUES(?)";
+        PreparedStatement ps = con.prepareStatement(query);
+        
+        // Mengisi tanda tanya (?) pertama dengan variabel name
+        ps.setString(1, name); 
 
-        Statement s = con.createStatement();
-
-        s.executeUpdate(query);
+        // 4. EKSEKUSI QUERY
+        ps.executeUpdate();
 
         System.out.println("Berhasil insert!");
 
-        s.close();
+        // 5. TUTUP RESOURCE
+        ps.close();
         con.close();
     }
 }
