5ec61d6601f358eb5ab7d39706a7f288893d3ac2
Rahmat Argyandha Aminuddin <argyandha25@gmail.com>
2026-05-28 23:23:39 +0700

    Pertemuan 13 nomor 4 file A

---
+++ PBO13_24060124130061_Nomor_4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo13_24060124130061_nomor_4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO13_24060124130061_Nomor_4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
